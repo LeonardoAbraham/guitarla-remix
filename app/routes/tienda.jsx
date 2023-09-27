796b0494c6cd9d23f3a6271db6714091e0d4879c
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-09-27 17:41:47 -0600

    Aplicando CSS al listado de Guitarras

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,32 @@
 import { useLoaderData } from "@remix-run/react"
 import { getGuitarras } from "../models/guitarras.server" 
 import Guitarra from "../components/guitarra"
+import styles from '../styles/guitarras.css'
+
+export function meta(){
+    return [
+        {
+            title: 'GuitarLA - Tienda de Guitarras'
+        },
+        {
+            property: "og:title",
+            content: "GuitarLA - Tienda de Guitarras",
+        },
+        {
+            name: "description",
+            content: "GuitarLA - Nuestra colección de guitarras",
+        },
+    ]
+}
+
+export function links(){
+    return [
+        {
+            rel:'stylesheet',
+            href:styles
+        }
+    ]
+}
 
 export async function loader(){
 
