00c8dea400f14b8e10a7d03ea4d75e3b469228c1
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-09-27 16:03:56 -0600

    CSS a la página de nosotros y otros atributos a Meta

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,4 +1,36 @@
 import imagen from '../../public/img/nosotros.jpg'
+import styles from '../styles/nosotros.css'
+
+export function meta(){
+    return [
+        {
+            title: 'GuitarLA - Sobre Nosotros'
+        },
+        {
+            property: "og:title",
+            content: "GuitarLA - Sobre Nosotros",
+        },
+        {
+            name: "description",
+            content: "Venta de guitarras, blog de música",
+        },
+    ]
+}
+
+export function links(){
+    return [
+        {
+            rel:'stylesheet',
+            href:styles
+        },
+        {
+            rel:'preload',
+            href:imagen,
+            as:'image'
+        }
+    ]
+}
+
 function Nosotros(){
     return (
         <main className="contenedor nosotros">
