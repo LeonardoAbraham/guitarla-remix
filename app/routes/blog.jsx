c601640ea14e6b4613aaf72021504534c36811d7
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-09-28 15:16:46 -0600

    Primeros pasos con el Blog

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,9 +1,31 @@
+import { useLoaderData } from "@remix-run/react";
+import { getPosts } from "../models/posts"
+import Post from "../components/post";
 
+export async function loader(){
+    const posts = await getPosts();
+    return posts.data
+}
 
 function Blog() {
-  return (
-    <div>blog</div>
-  )
+
+    const posts = useLoaderData();
+
+    return (
+        <main className="contenedor">
+            <h2 className="heading">Blog</h2>
+            <div className="blog">
+                {
+                    posts.map(post => (
+                        <Post 
+                            key={post.id}
+                            post={post.attributes}
+                        />
+                    ))
+                }
+            </div>
+        </main>
+    )
 }
 
 export default Blog
