1b99417e9bdbaced8bb89cd98aa463eeb133d079
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-10-02 14:57:38 -0600

    Remix avanzado - Nested Routes para Blog

--- app/routes/blog.$postUrl.jsx
+++ app/routes/blog.$postUrl.jsx
@@ -1,7 +1,6 @@
 import { useLoaderData } from "@remix-run/react";
 import { getPost } from "../models/posts.server"
 import { formatearFecha } from "../utils/helpers";
-import styles from "../styles/blog.css"
 
 export function meta({data}){
     if(!data){
@@ -26,14 +25,6 @@ export function meta({data}){
     ]
 }
 
-export function links(){
-    return [
-        {
-            rel:'stylesheet',
-            href:styles
-        }
-    ]
-}
 
 export async function loader({params}){
     const { postUrl } = params;
@@ -55,7 +46,7 @@ export default function Post() {
     const { titulo, contenido, imagen, publishedAt } = post.data[0].attributes;
 
     return (
-        <article className="contenedor post mt-3">
+        <article className="post mt-3">
             <img className="imagen" src={imagen.data.attributes.url} alt={`Imagen blog ${titulo}`} />
             <div className="contenido">
                 <h3>{titulo}</h3>
--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,19 +1,5 @@
-import { useLoaderData } from "@remix-run/react";
-import { getPosts } from "../models/posts.server"
-import ListadoPosts from "../components/listado-posts";
+import { Outlet } from "@remix-run/react";
 import styles from "../styles/blog.css"
-
-export function meta(){
-    return [
-        {
-            title: 'GuitarLA - Nuestro Blog'
-        },
-        {
-            name: "description",
-            content: "GuitarLA, Blog de música y venta de guitarras",
-        },
-    ]
-}
 
 export function links(){
     return [
@@ -24,20 +10,12 @@ export function links(){
     ]
 }
 
-export async function loader(){
-    const posts = await getPosts();
-    return posts.data
-}
 
 function Blog() {
 
-    const posts = useLoaderData();
-
     return (
         <main className="contenedor">
-            <ListadoPosts 
-                posts={posts}
-            />
+            <Outlet />
         </main>
     )
 }
