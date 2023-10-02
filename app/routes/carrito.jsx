64e674db185d40bf7155ad1bedc41326253854a5
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-10-02 17:12:12 -0600

    Carrito de compras - añadiendo el contenido y CSS

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,8 +1,44 @@
 
+import styles from '../styles/carrito.css'
+
+export function links(){
+    return [
+        {
+            rel:'stylesheet',
+            href:styles
+        }
+    ]
+}
+
+export function meta(){
+    return [
+        {
+            title: 'GuitarLA - Carrito de Compras'
+        },
+        {
+            name: "description",
+            content: "GuitarLA - Venta de guitarras, música, blog, carrito de compras, tienda",
+        },
+    ]
+}
 
 const Carrito = () => {
     return (
-        <div>carrito</div>
+        <main className="contenedor">
+            <h1 className="heading">Carrito de Compras</h1>
+
+            <div className="contenido">
+                <div className="carrito">
+                    <h2>Articulos</h2>
+                </div>
+                <aside className="resumen">
+                    <h3>Resumen del Pedido</h3>
+                    <p>Total a pagar: $</p>
+                </aside>
+            </div>
+
+            
+        </main>
     )
 }
 
