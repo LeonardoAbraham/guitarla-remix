a6f51d2819949ac79831d30311074520c0d58b0f
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-10-02 11:36:30 -0600

    Mostrar el Blog en la página principal

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,7 +2,9 @@ import { useLoaderData } from '@remix-run/react'
 import { getGuitarras } from '../models/guitarras.server'
 import { getPosts } from '../models/posts.server'
 import ListadoGuitarras from '../components/listado-guitarras'
+import ListadoPosts from '../components/listado-posts'
 import stylesGuitarras from '../styles/guitarras.css'
+import stylesPosts from '../styles/blog.css'
 
 export function meta(){
 
@@ -13,7 +15,11 @@ export function links(){
         {
             rel: 'stylesheet',
             href: stylesGuitarras
-        }
+        },
+        {
+            rel: 'stylesheet',
+            href: stylesPosts
+        },
     ]
 }
 
@@ -40,6 +46,12 @@ function Index(){
                     guitarras={guitarras}
                 />
             </main>
+
+            <section className='contenedor'>
+                <ListadoPosts 
+                    posts={posts}
+                />
+            </section>
         </>
     )
 }
