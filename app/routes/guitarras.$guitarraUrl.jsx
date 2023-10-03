aebeafeb441942ff042a567c5488916a6e632b87
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-10-03 11:19:11 -0600

    Validando la acción de añadir al carrito

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -46,7 +46,24 @@ const Guitarra = () => {
 
     const guitarra = useLoaderData();
     
-    const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes
+    const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if(cantidad < 1){
+            alert('Debes seleccionar una cantidad')
+            return;
+        }
+
+        const guitarraSeleccionada = {
+            id: guitarra.data[0].id,
+            imagen: imagen.data.attributes.url,
+            nombre,
+            precio,
+            cantidad
+        }
+    }
 
     return (
         <div className="guitarra">
@@ -57,13 +74,13 @@ const Guitarra = () => {
                 <p className="texto">{descripcion}</p>
                 <p className="precio">${precio}</p>
 
-                <form className="formulario">
+                <form onSubmit={handleSubmit} className="formulario">
                     <label htmlFor="cantidad">Cantidad</label>
                     <select 
                         onChange={ e => setCantidad(parseInt(e.target.value)) }
                         id="cantidad"
                     >
-                        <option value="">-- Seleccione --</option>
+                        <option value="0">-- Seleccione --</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
